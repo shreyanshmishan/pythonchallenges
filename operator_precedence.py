d422a67ee01eaa2253a77eefadca6c0aa348f794
shreyanshmishan <shreyanshm.ishan@gmail.com>
2026-07-08 15:32:18 -0400

    Add files via upload

---
+++ operator_precedence.py
@@ -0,0 +1,16 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y
+print(f"the value of z is {z}")
+a=0
+a=(w*v)/y+(z**(w+v))
+print("the value of a is", a)
+name="shreyansh"
+age=11
+if name=="shreyansh" or name=="nikhil" and age<=10:
+    print("hello,welcome")
+else:
+    print("goodbye")
